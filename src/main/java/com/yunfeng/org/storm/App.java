245dc3800c7bd958111bb1fa8b30199b875277d3
wuyunfeng <wuyunfeng@f-road.com.cn>
2017-02-02 11:24:49 +0800

    initial commit!

---
+++ src/main/java/com/yunfeng/org/storm/App.java
@@ -0,0 +1,13 @@
+package com.yunfeng.org.storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
